584ddcf17895b459562e4dc8f33f15821321c8b2
yennanliu <f339339@gmail.com>
2020-12-21 22:20:38 +0800

    add spark code, data/

---
+++ spark/src/main/scala/com/yen/dev/HelloWorld.scala
@@ -0,0 +1,9 @@
+package com.yen.dev
+
+object HelloWorld extends App{
+  println("*** this is hello worldddd !!!")
+
+  for (i <- 1 to 10){
+    println(i)
+  }
+}
---
+++ spark/src/main/scala/com/yen/dev/SreamFromFile.scala
@@ -0,0 +1,39 @@
+package com.yen.dev
+
+import java.util.concurrent.TimeUnit
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+// https://github.com/yennanliu/NYC_Taxi_Pipeline/blob/master/src/main/scala/EventLoad/FileStreamExample.scala
+
+object StreamFromFile extends App{
+
+  val sparkSession = SparkSession.builder
+    .master("local")
+    .appName("StreamFromFile")
+    .getOrCreate()
+
+  val schema = StructType(
+    Array(StructField("id", StringType),
+      StructField("name", StringType),
+      StructField("amount", StringType))
+  )
+
+  // stream from file
+  val streamDF = sparkSession
+    .readStream
+    .option("header", "true")
+    .schema(schema)
+    .csv("../data/dev")
+
+  // show in console
+  val query = streamDF
+    .writeStream
+    .format("console")
+    .outputMode(OutputMode.Append())
+    .start()
+
+  query.awaitTermination()
+}
