1998c97555c3c9933c2a67ea6ab1fe1f9114e5d1
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-01-16 09:43:13 +0800

    add spark-avro build.sbt, add yen/dev/KafkaAvroSinkDemo1.scala

--- spark/build.sbt
+++ spark/build.sbt
@@ -4,21 +4,24 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-val sparkVersion = "2.3.0"
+val sparkVersion = "2.4.3"
 
 resolvers += Resolver.sonatypeRepo("snapshots")
 
 libraryDependencies ++= Seq(
   // config
   "com.typesafe" % "config" % "1.2.1", 
-  "org.apache.spark" %% "spark-core" % "2.4.3",
+  "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.scalactic" %% "scalactic" % "3.1.0",
-  "org.apache.spark" %% "spark-sql" % "2.4.3",
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
   "com.databricks" %% "spark-csv" % "1.4.0",
 
   // spark stream 
-   "org.apache.spark" %% "spark-streaming" % "2.4.3",
-   "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.4.3",
+   "org.apache.spark" %% "spark-streaming" % sparkVersion,
+   "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion,
+
+  // spark-avro
+  "org.apache.spark" %% "spark-avro" % sparkVersion,
 
   // others 
   "org.apache.commons" % "commons-text" % "1.8",
---
+++ spark/src/main/scala/com/yen/dev/KafkaAvroSinkDemo1.scala
@@ -0,0 +1,105 @@
+package com.yen.dev
+
+// https://github.com/yennanliu/KafkaSparkPoc/blob/main/exampleCode/Spark-Streaming-In-Scala-master/06-KafkaAvroSinkDemo/src/main/scala/guru/learningjournal/spark/examples/KafkaAvroSinkDemo.scala
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.avro.to_avro //.functions.to_avro
+import org.apache.spark.sql.functions.{col, expr, from_json, struct}
+import org.apache.spark.sql.types._
+
+object KafkaAvroSinkDemo1 extends App{
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  val spark = SparkSession.builder()
+    .master("local[3]")
+    .appName("Kafka Avro Sink Demo")
+    .config("spark.streaming.stopGracefullyOnShutdown", "true")
+    .getOrCreate()
+
+  // *** define schema for stream df
+  val schema = StructType(
+    List(
+      StructField("InvoiceNumber", StringType),
+      StructField("CreatedTime", LongType),
+      StructField("StoreID", StringType),
+      StructField("PosID", StringType),
+      StructField("CashierID", StringType),
+      StructField("CustomerType", StringType),
+      StructField("CustomerCardNo", StringType),
+      StructField("TotalAmount", DoubleType),
+      StructField("NumberOfItems", IntegerType),
+      StructField("PaymentMethod", StringType),
+      StructField("CGST", DoubleType),
+      StructField("SGST", DoubleType),
+      StructField("CESS", DoubleType),
+      StructField("DeliveryType", StringType),
+      StructField(
+        "DeliveryAddress",
+        StructType(
+          List(
+            StructField("AddressLine", StringType),
+            StructField("City", StringType),
+            StructField("State", StringType),
+            StructField("PinCode", StringType),
+            StructField("ContactNumber", StringType)
+          )
+        )
+      ),
+      StructField(
+        "InvoiceLineItems",
+        ArrayType(
+          StructType(
+            List(
+              StructField("ItemCode", StringType),
+              StructField("ItemDescription", StringType),
+              StructField("ItemPrice", DoubleType),
+              StructField("ItemQty", IntegerType),
+              StructField("TotalValue", DoubleType)
+            )
+          )
+        )
+      )
+    )
+  )
+
+  val kafkaTopic = "invoices_avro"
+  val kafkaSourceDF = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", kafkaTopic)
+    .option("startingOffsets", "earliest")
+    .load()
+
+  val valueDF = kafkaSourceDF.select(from_json(col("value").cast("string"), schema).alias("value"))
+
+  val explodeDF = valueDF.selectExpr("value.InvoiceNumber", "value.CreatedTime", "value.StoreID",
+    "value.PosID", "value.CustomerType", "value.CustomerCardNo", "value.DeliveryType", "value.DeliveryAddress.City",
+    "value.DeliveryAddress.State", "value.DeliveryAddress.PinCode", "explode(value.InvoiceLineItems) as LineItem")
+
+  val flattenedDF = explodeDF
+    .withColumn("ItemCode", expr("LineItem.ItemCode"))
+    .withColumn("ItemDescription", expr("LineItem.ItemDescription"))
+    .withColumn("ItemPrice", expr("LineItem.ItemPrice"))
+    .withColumn("ItemQty", expr("LineItem.ItemQty"))
+    .withColumn("TotalValue", expr("LineItem.TotalValue"))
+    .drop("LineItem")
+
+  val kafkaTargetDF = flattenedDF.select(expr("InvoiceNumber as key"),
+    to_avro(struct("*")).alias("value"))
+
+  val outPutKafkaTopic = "invoice-avro-items"
+  val invoiceWriterQuery = kafkaTargetDF
+    .writeStream
+    .queryName("Flattened Invoice Writer")
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("topic", outPutKafkaTopic)
+    .outputMode("append")
+    .option("checkpointLocation", "chk-point-dir")
+    .start()
+
+  logger.info("Start Writer Query")
+  invoiceWriterQuery.awaitTermination()
+}
