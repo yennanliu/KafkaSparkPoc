e19b0d9b72ae62627cb1cd669acad95b501b30c9
yennanliu <f339339@gmail.com>
2021-01-17 14:10:16 +0800

    backup data, code

---
+++ spark/src/main/scala/com/yen/SampleApp01/InvoiceGenerator.java
@@ -0,0 +1,19 @@
+package com.yen.SampleApp01;
+
+// https://github.com/yennanliu/Apache-Kafka-For-Absolute-Beginners/blob/master/07-pos-simulator-completed/src/main/java/guru/learningjournal/kafka/examples/datagenerator/InvoiceGenerator.java
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+//import guru.learningjournal.kafka.examples.types.DeliveryAddress;
+//import guru.learningjournal.kafka.examples.types.PosInvoice;
+//import guru.learningjournal.kafka.examples.types.LineItem;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class InvoiceGenerator {
+    //System.out.println("hello worldddd JAVA");
+}
---
+++ spark/src/main/scala/com/yen/dev/HelloWorldJava.java
@@ -0,0 +1,10 @@
+package com.yen.dev;
+
+public class HelloWorldJava {
+    public static void main(String[] args){
+        System.out.println("hello worldddd JAVA");
+        for (int i =0; i<= 10; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
