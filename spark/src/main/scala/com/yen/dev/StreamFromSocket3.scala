9e433796201a5b7a1ce1f72cb6edfb11ecb4ea9f
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-06 16:29:07 +0800

    add StreamFromSocket3.scala, update StreamFromSocket2.scala

--- spark/src/main/scala/com/yen/dev/StreamFromSocket3.scala
+++ spark/src/main/scala/com/yen/dev/StreamFromSocket3.scala
@@ -7,12 +7,12 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object StreamFromSocket2 extends App {
+object StreamFromSocket3 extends App {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
   val spark = SparkSession.builder()
     .master("local[3]")
-    .appName("StreamFromSocket2")
+    .appName("StreamFromSocket3")
     .config("spark.streaming.stopGracefullyOnShutdown", "true")
     .config("spark.sql.shuffle.partitions", 3)
     .getOrCreate()
@@ -30,15 +30,28 @@ object StreamFromSocket2 extends App {
   println("***")
   wordsDF.printSchema()
 
-  // let's filter the df here!
-  // https://spark.apache.org/docs/latest/structured-streaming-programming-guide.html
-  // https://stackoverflow.com/questions/35759099/filter-spark-dataframe-on-string-contains
-  val filterDF = wordsDF
-      .filter($"word".contains("!@?#"))
-      //.filter("word like '#'")
-      //.filter("word > 10")
+  // let's replace the text in df here!
+  // https://stackoverflow.com/questions/40080609/how-to-use-regexp-replace-in-spark
+  // Note : have to "escape the string"
+  // https://stackoverflow.com/questions/13948751/string-parse-error/13949358
+  // e.g. indead of sysCallName.split("("), do sysCallName.split("\\(");
+  // TODO : fix below workaround (e.g. can we do multiple "regexp_replace" in one df ?)
+  val filterDF1 = wordsDF
+    .withColumn("word", regexp_replace(wordsDF("word"), "\\(", ""))
 
-  val countsDF = filterDF.groupBy("word").count()
+  val filterDF2 = filterDF1
+    .withColumn("word", regexp_replace(filterDF1("word"), "\\(", ""))
+
+  val filterDF3 = filterDF2
+    .withColumn("word", regexp_replace(filterDF2("word"), "\\?", ""))
+
+  val filterDF4 = filterDF3
+    .withColumn("word", regexp_replace(filterDF3("word"), "\\*", ""))
+
+  println("***")
+  filterDF4.printSchema()
+
+  val countsDF = filterDF4.groupBy("word").count()
 
   val wordCountQuery = countsDF.writeStream
     .format("console")
@@ -49,5 +62,4 @@ object StreamFromSocket2 extends App {
 
   logger.info("Listening to localhost:9999")
   wordCountQuery.awaitTermination()
-
 }
