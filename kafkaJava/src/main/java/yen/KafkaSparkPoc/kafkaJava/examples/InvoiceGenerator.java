dba1acb3d4f39ac4fbc9a48a1ee0d73f1346efda
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-16 19:16:21 +0800

    update examples/InvoiceGenerator.java

--- kafkaJava/src/main/java/yen/KafkaSparkPoc/kafkaJava/examples/InvoiceGenerator.java
+++ kafkaJava/src/main/java/yen/KafkaSparkPoc/kafkaJava/examples/InvoiceGenerator.java
@@ -17,5 +17,73 @@ import java.util.Random;
 public class InvoiceGenerator {
     private static final Logger logger = LogManager.getLogger();
     private static InvoiceGenerator ourInstance = new InvoiceGenerator();
+    private final Random invoiceIndex;
+    private final Random invoiceNumber;
+    private final Random numberOfItems;
+    private final PosInvoice[] invoices;
 
+    public static InvoiceGenerator getInstance(){
+        return ourInstance;
+    }
+
+    private InvoiceGenerator(){
+        String DATAFILE = "src/main/resources/data/Invoice.json";
+        ObjectMapper mapper;
+        invoiceIndex = new Random();
+        invoiceNumber = new Random();
+        numberOfItems = new Random();
+        mapper = new ObjectMapper();
+
+        try {
+            invoices = mapper.readValue(new File(DATAFILE), PosInvoice[].class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int getIndex(){
+        return invoiceIndex.nextInt(100);
+    }
+
+    private int getNewInvoiceNumber() {
+        return invoiceNumber.nextInt(99999999) + 99999;
+    }
+
+    private int getNoOfItems() {
+        return numberOfItems.nextInt(4) + 1;
+    }
+
+    public PosInvoice getNextInvoice() {
+        PosInvoice invoice = invoices[getIndex()];
+        invoice.setInvoiceNumber(Integer.toString(getNewInvoiceNumber()));
+        invoice.setCreatedTime(System.currentTimeMillis());
+        if ("HOME-DELIVERY".equalsIgnoreCase(invoice.getDeliveryType())) {
+
+            // TO IMPLEMENT : AddressGenerator
+            DeliveryAddress deliveryAddress = AddressGenerator.getInstance().getNextAddress();
+            invoice.setDeliveryAddress(deliveryAddress);
+        }
+        int itemCount = getNoOfItems();
+        Double totalAmount = 0.0;
+        List<LineItem> items = new ArrayList<>();
+
+        // TO IMPLEMENT : ProductGenerator
+        ProductGenerator productGenerator = ProductGenerator.getInstance();
+
+        for (int i = 0; i < itemCount; i++){
+            LineItem item = ProductGenerator.getNextProduct();
+            totalAmount += item.getTotalValue();
+            items.add(item);
+        }
+
+        invoice.setNumberOfItems(itemCount);
+        invoice.setInvoiceLineItems(items);
+        invoice.setTotalAmount(totalAmount);
+        invoice.setTaxableAmount(totalAmount);
+        invoice.setCGST(totalAmount * 0.025);
+        invoice.setSGST(totalAmount * 0.025);
+        invoice.setCESS(totalAmount * 0.00125);
+        logger.debug(invoice);
+        return invoice;
+    }
 }
