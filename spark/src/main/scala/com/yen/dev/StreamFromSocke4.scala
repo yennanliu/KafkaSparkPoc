9ef4ce6d88058a501ce06ca5f8e4a271d8a260fe
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-04-23 16:32:50 +0800

    add dev/StreamFromSocke4.scala

---
+++ spark/src/main/scala/com/yen/dev/StreamFromSocke4.scala
@@ -0,0 +1,48 @@
+package com.yen.dev
+
+// spark streaming from socket data source
+// https://github.com/yennanliu/Spark-Streaming-In-Scala/tree/master/01-StreamingWC
+
+// plz open the other terminal an run below command as socket first
+// nc -lk 9999
+
+import com.yen.streamToHDFS.streamSocketEventToHDFS.lines
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+// TODO : check how to run with "Serializable"
+//object StreamFromSocket extends Serializable
+object StreamFromSocket4 extends App{
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  val spark = SparkSession.builder()
+    .master("local[3]")
+    .appName("StreamFromSocket")
+    .config("spark.streaming.stopGracefullyOnShutdown", "true")
+    .config("spark.sql.shuffle.partitions", 3)
+    .getOrCreate()
+
+  val linesDF = spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", "9999")
+    .load()
+
+  //val wordsDF = linesDF.select(explode(split(col("value"), " ")).alias("word"))
+  val wordsDF = linesDF.select(
+    expr("explode(split(value,' ')) as word"),
+    date_format(col("current_timestamp"),"HH:mm:ss.SSS").as("time")
+  )
+
+  val countsDF = wordsDF.groupBy("word").count()
+
+  val wordCountQuery = wordsDF.writeStream
+    .format("console")
+    //.option("numRows", 2)
+    .option("checkpointLocation", "chk-point-dir") // set up the "checkpoint"
+    .start()
+
+  logger.info("Listening to localhost:9999")
+  wordCountQuery.awaitTermination()
+}
