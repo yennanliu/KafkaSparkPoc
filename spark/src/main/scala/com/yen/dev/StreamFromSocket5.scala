196e8c1c0a3897e04ff5d13f90202cb123865822
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-04-26 14:03:34 +0800

    read event with schema, update comment (dev/StreamFromSocket5.scala

--- spark/src/main/scala/com/yen/dev/StreamFromSocket5.scala
+++ spark/src/main/scala/com/yen/dev/StreamFromSocket5.scala
@@ -4,17 +4,18 @@ package com.yen.dev
  * StreamFromSocket5
  *
  * 1) spark job digest event generated from com.yen.eventCreator.SimpleEvent_V1
- * 2) will read the socket event with schema and print output in console
- * 3) ref : https://github.com/yennanliu/KafkaSparkPoc/blob/main/spark/src/main/scala/com/yen/eventCreator/SimpleEvent_V1.scala
- *
+ * 2) input data : json, output data : N/A
+ * 3) will read the socket event with schema and print output in console
+ * 4) ref : https://github.com/yennanliu/KafkaSparkPoc/blob/main/spark/src/main/scala/com/yen/eventCreator/SimpleEvent_V1.scala
  */
 
-// plz open the other terminal an run below command as socket first
-// nc -lk 9999
+// plz run com.yen.eventCreator.SimpleEvent_V1 as event-sending socket
+
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
 
 object StreamFromSocket5 extends App {
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
@@ -26,17 +27,32 @@ object StreamFromSocket5 extends App {
     .config("spark.sql.shuffle.partitions", 3)
     .getOrCreate()
 
+  val eventSchema = StructType(List(
+    StructField("id", IntegerType),
+    StructField("event_date", LongType),
+    StructField("msg", StringType)
+  ))
+
   val lineDF = spark.readStream
     .format("socket")
     .option("host", "localhost")
     .option("port", "9999")
     .load()
 
-  val wordsDF = lineDF.select(expr("explode(split(value,' ')) as word"))
+  val  valueDF = lineDF.select(
+    from_json(col("value").cast("string"), eventSchema)
+      .alias("value")
+  )
 
-  lineDF.printSchema()
+  val eventDF = valueDF.select("value.*")
+    //.withColumn("CreatedTime", to_timestamp(col("CreatedTime"), "yyyy-MM-dd HH:mm:ss"))
+    .withColumn("id", expr("id"))
+    .withColumn("event_date", expr("event_date"))
+    .withColumn("msg", expr("msg"))
 
-  val query = lineDF.writeStream
+  eventDF.printSchema()
+
+  val query = eventDF.writeStream
     .format("console")
     .option("checkpointLocation", "chk-point-dir")
     .start()
