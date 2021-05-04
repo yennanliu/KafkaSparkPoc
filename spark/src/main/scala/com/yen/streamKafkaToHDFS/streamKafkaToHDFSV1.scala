2fa74f9a2fa359275dbd3239107f42d6fe24b0b5
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-05-04 09:35:03 +0800

    add scala/com/yen/streamKafkaToHDFS/

---
+++ spark/src/main/scala/com/yen/streamKafkaToHDFS/streamKafkaToHDFSV1.scala
@@ -0,0 +1,51 @@
+package com.yen.streamKafkaToHDFS
+
+// event source :
+// https://github.com/yennanliu/KafkaSparkPoc/blob/main/kafka/src/main/scala/com/yen/Producer/producerV1.scala
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.SparkContext
+
+object streamKafkaToHDFSV1 extends App {
+
+  @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
+
+  val spark = SparkSession
+    .builder
+    .appName(this.getClass.getName)
+    .master("local[*]")
+    .config("spark.sql.warehouse.dir", "/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
+    .getOrCreate()
+
+  import spark.implicits._
+
+  // kafka config
+  val bootStrapServers = "127.0.0.1:9092"
+  val topic = "raw_data"
+
+  // subscribe to topic
+  val streamDF = spark
+    .readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", bootStrapServers)
+    .option("subscribe", topic)
+    .load()
+
+  val tmpStreamDF = streamDF.selectExpr("CAST(value AS STRING)")
+
+  val ToStreamDF = tmpStreamDF
+    .select("value")
+
+  ToStreamDF.createOrReplaceTempView("to_stream")
+
+  val query = ToStreamDF.writeStream
+    .format("console")
+    .option("checkpointLocation", "chk-point-dir2")
+    .start()
+
+  logger.info("Listen from Kafka : 127.0.0.1:9092")
+
+  query.awaitTermination()
+}
