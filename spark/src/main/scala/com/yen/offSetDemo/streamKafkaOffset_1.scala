f77b5c9b84a6461a080907754274cd8b72691f47
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-05-19 11:42:13 +0800

    update event cols, fix output path (streamKafkaOffset_1.scala

--- spark/src/main/scala/com/yen/offSetDemo/streamKafkaOffset_1.scala
+++ spark/src/main/scala/com/yen/offSetDemo/streamKafkaOffset_1.scala
@@ -49,19 +49,20 @@ object streamKafkaOffset_1 extends App {
     "CAST(key AS STRING)",
     "CAST(value AS STRING)",
     "CAST(partition AS INT)",
-    "CAST(offset AS INT)"
+    "CAST(offset AS INT)",
+    "CAST(timestamp AS timestamp)"
   )
 
   val ToStreamDF = tmpStreamDF
-    .select("topic","key", "value", "partition", "offset")
+    .select("topic","key", "value", "partition", "offset", "timestamp")
 
   //ToStreamDF.createOrReplaceTempView("to_stream")
 
   /** V1 : print out in console */
-    val query = ToStreamDF.writeStream
-          .format("console")
-          .option("checkpointLocation", "chk-point-dir2")
-          .start()
+  //    val query = ToStreamDF.writeStream
+  //          .format("console")
+  //          .option("checkpointLocation", "chk-point-dir2")
+  //          .start()
 
   /** V2 : save into HDFS */
   //   val query = ToStreamDF
@@ -69,18 +70,18 @@ object streamKafkaOffset_1 extends App {
   //          .partitionBy("topic") // save data partition by column : http://spark.apache.org/docs/2.1.1/api/java/org/apache/spark/sql/streaming/DataStreamWriter.html
   //          .format("json")
   //          .option("checkpointLocation", "chk-point-dir2")
-  //          .option("path", s"streamKafkaToHDFSV1")
+  //          .option("path", s"streamKafkaOffset_1")
   //          .start()
 
   /** V3 : save into HDFS with compression */
-//  val query = ToStreamDF
-//    .writeStream
-//    .partitionBy("topic")
-//    .format("json")
-//    .option("checkpointLocation", "chk-point-dir2")
-//    .option("path", "streamKafkaToHDFSV1")
-//    .option("compression", "org.apache.hadoop.io.compress.BZip2Codec") // https://sites.google.com/a/einext.com/einext_original/apache-spark/compress-output-files-in-spark
-//    .start()
+  val query = ToStreamDF
+    .writeStream
+    .partitionBy("topic")
+    .format("json")
+    .option("checkpointLocation", "chk-point-dir2")
+    .option("path", "streamKafkaOffset_1")
+    .option("compression", "org.apache.hadoop.io.compress.BZip2Codec") // https://sites.google.com/a/einext.com/einext_original/apache-spark/compress-output-files-in-spark
+    .start()
 
   logger.info("Listen from Kafka : 127.0.0.1:9092")
 
