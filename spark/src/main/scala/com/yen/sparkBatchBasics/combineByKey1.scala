f37fded7639d35c3d43e08d34a4d08a3f3991104
yennanliu <f339339@gmail.com>
2021-07-02 08:28:05 +0800

    update

--- spark/src/main/scala/com/yen/sparkBatchBasics/combineByKey1.scala
+++ spark/src/main/scala/com/yen/sparkBatchBasics/combineByKey1.scala
@@ -29,18 +29,25 @@ object combineByKey1 extends App {
 
   val rdd1 = sc.parallelize(Array(("a",88), ("b",95), ("a",91), ("b", 93), ("b", 98)))
 
+  // ----------------------------
   // Step 1 : get sum per key
+  // ----------------------------
 
   // wrong
   //val r1 = rdd1.combineByKey(v => (v,1), (c,v)=>(c._1+v, c._2+1), (c1,c2) => (c1._1 + c2._1, c1._2 + c2._2))
 
   // correct
-  val r1 = rdd1.combineByKey( (_,1), (acc:(Int, Int),v) =>
-  (acc._1+v, acc._2 +1 ), (acc1:(Int,Int), acc2:(Int,Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2))
+  val r1 = rdd1.combineByKey(
+     (_,1), (acc:(Int, Int),v) => (acc._1 + v, acc._2 +1 ),
+     (acc1:(Int,Int), acc2:(Int,Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+  )
 
   println("r1 = " + r1.collect().toList)
 
-  // Step 1 : get average per key
+  // ----------------------------
+  // Step 2 : get average per key
+  // ----------------------------
+
   val r1_ = r1.map(x => (x._1, x._2._1 / x._2._2 ))
   println("r1_ = " + r1_.collect().toList)
 
