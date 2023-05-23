f48d7ef08a5e83cc8bfd0a4c3b4015c0df743890
Yen Liu <yenanliu@amazon.com>
2023-05-23 22:44:36 +0100

    update script

--- spark/src/main/scala/com/yen/poc1/test1.scala
+++ spark/src/main/scala/com/yen/poc1/test1.scala
@@ -2,6 +2,9 @@ package com.yen.poc1
 
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions.explode
+import scala.reflect.internal.util.TableDef.Column
 
 object test1 extends App {
 
@@ -14,9 +17,54 @@ object test1 extends App {
   // load from s3
   val s3_bucket = "s3://xxx/yyy"
   val nested: sql.DataFrame = spark.read.option("multiline", true).json(s3_bucket)
+  // TODO: fix type ?
+  val flatten_df = nested
+    .select(explode($"hostVulnerabilityList").as("exploded"))
+    .select("exploded.*")
 
-  nested.printSchema
+  flatten_df.printSchema
+
+  // set up config
+  val jdbc_iam_url = "jdbc:redshift:iam://redshift-spark-redshift-cluster.zzzz.us-east-1.redshift.amazonaws.com:5439/dev"
+  val temp_dir = "s3://redshift-spark-databucket-zzzz/tmp/"
+  val aws_role = "arn:aws:iam::123:role/redshift-spark-RedshiftIamRole-123"
+
+  // Set query group for the query
+  val queryGroup = "spark-redshift-emr"
+  val jdbc_iam_url_withQueryGroup = jdbc_iam_url + "?queryGroup=" + queryGroup
+
+  // Set User name for the query
+  val userName = "redshiftmasteruser"
+  val jdbc_iam_url_withUserName = jdbc_iam_url + "?user=" + userName
+
+
+  val redshiftOptions = Map(
+    "url" -> jdbc_iam_url_withUserName,
+    "tempdir" -> temp_dir,
+    "aws_iam_role" -> aws_role
+  )
+
 
   // write to redshift
+  // save to redshift
+  flatten_df.write
+    .format("io.github.spark_redshift_community.spark.redshift")
+    .option("url", jdbc_iam_url_withUserName)
+    .option("dbtable", "nested")
+    .option("tempdir", temp_dir)
+    .option("aws_iam_role", aws_role)
+    .mode("error")
+    .save()
+
+  // check
+  val flatten_df_dump = (
+    spark.read
+      .format("io.github.spark_redshift_community.spark.redshift")
+      .options(redshiftOptions)
+      .option("dbtable", "nested")
+      .load()
+    )
+
+  flatten_df_dump.show()
 
 }
